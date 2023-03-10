65c9f5b37335d7888fd7b0831691ad2377b5996b
Dennis Snell <dennis.snell@automattic.com>
2023-03-09 17:47:18 -0700

    Add the rest of it

--- src/wp-includes/html-api/class-wp-html-processor.php
+++ src/wp-includes/html-api/class-wp-html-processor.php
@@ -43,6 +43,14 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 			return false;
 		}
 
+		if ( 0 < count( $this->open_elements ) ) {
+			$element = WP_HTML_Spec::element_info( end( $this->open_elements ) );
+			// @TODO: Handle self-closing HTML foreign elements: must convey self-closing flag on stack.
+			if ( $element::is_void ) {
+				array_pop( $this->open_elements );
+			}
+		}
+
 		if ( false === parent::next_tag( array( 'tag_closers' => 'visit' ) ) ) {
 			return false;
 		}
@@ -52,6 +60,7 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 
 		$self_closes = $element::is_void || ( ! $element::is_html && $this->has_self_closing_flag() );
 		if ( $self_closes ) {
+			$this->open_elements[] = $tag_name;
 			return true;
 		}
 
@@ -65,33 +74,186 @@ class WP_HTML_Processor extends WP_HTML_Tag_Processor {
 	}
 
 	public function next_sibling() {
-		if ( $this->fully_supported_input ) {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
 			return false;
 		}
 
 		$starting_depth = count( $this->open_elements );
 
-		/*
-		 * If we aren't already inside a tag then advance to the first one.
-		 * If that tag is self-closing then we're done. Otherwise, open the
-		 * stack with that tag name and prepare to close out the stack.
-		 */
-		if ( ! $this->get_tag() ) {
-			if ( ! $this->next_tag() ) {
-				return false;
+		while ( $this->next_tag() ) {
+			$current_depth = count( $this->open_elements );
+
+			if ( ! $this->is_tag_closer() && $current_depth === $starting_depth ) {
+				return true;
 			}
 
-			if ( $starting_depth === count( $this->open_elements ) ) {
-				return true;
+			if ( ! $this->is_tag_closer() && $current_depth < $starting_depth ) {
+				return false;
 			}
 		}
 
+		return false;
+	}
+
+	public function next_child() {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
+			return false;
+		}
+
+		$starting_depth = count( $this->open_elements );
+
 		while ( $this->next_tag() ) {
-			if ( $starting_depth === count( $this->open_elements ) ) {
+			$current_depth = count( $this->open_elements );
+
+			if ( ! $this->is_tag_closer() && $current_depth === $starting_depth + 1 ) {
 				return true;
 			}
 		}
 
 		return false;
+	}
+
+	private function find_closing_tag() {
+		$starting_depth = count( $this->open_elements );
+
+		while ( $this->next_tag() ) {
+			$current_depth = count( $this->open_elements );
+
+			if ( $this->is_tag_closer() && $current_depth < $starting_depth ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function get_inner_content() {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
+			return false;
+		}
+
+		if ( ! $this->get_tag() || $this->is_tag_closer() ) {
+			return false;
+		}
+
+		$element = WP_HTML_Spec::element_info( $this->get_tag() );
+		if ( $element::is_void || ( ! $element::is_html && $this->has_self_closing_flag() ) ) {
+			return false;
+		}
+
+		// @TODO: Unique bookmark names
+		$this->set_bookmark( 'start' );
+		if ( ! $this->find_closing_tag() ) {
+			return false;
+		}
+		$this->set_bookmark( 'end' );
+
+		$start = $this->bookmarks['start']->end + 1;
+		$end = $this->bookmarks['end']->start - 1;
+		$inner_content = substr( $this->html, $start, $end - $start + 1 );
+
+		$this->release_bookmark( 'start' );
+		$this->release_bookmark( 'end' );
+
+		return $inner_content;
+	}
+
+	public function set_inner_content( $new_html ) {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
+			return false;
+		}
+
+		if ( ! $this->get_tag() || $this->is_tag_closer() ) {
+			return false;
+		}
+
+		$element = WP_HTML_Spec::element_info( $this->get_tag() );
+		if ( $element::is_void || ( ! $element::is_html && $this->has_self_closing_flag() ) ) {
+			return false;
+		}
+
+		// @TODO: Unique bookmark names
+		$this->set_bookmark( 'start' );
+		if ( ! $this->find_closing_tag() ) {
+			return false;
+		}
+		$this->set_bookmark( 'end' );
+
+		$start = $this->bookmarks['start']->end + 1;
+		$end = $this->bookmarks['end']->start;
+		$this->lexical_updates[] = new WP_HTML_Text_Replacement( $start, $end, $new_html );
+		$this->get_updated_html();
+		$this->seek( 'start' );
+
+		$this->release_bookmark( 'start' );
+		$this->release_bookmark( 'end' );
+	}
+
+	public function get_outer_content() {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
+			return false;
+		}
+
+		if ( ! $this->get_tag() || $this->is_tag_closer() ) {
+			return false;
+		}
+
+		$element = WP_HTML_Spec::element_info( $this->get_tag() );
+		if ( $element::is_void || ( ! $element::is_html && $this->has_self_closing_flag() ) ) {
+			$this->set_bookmark( 'start' );
+			$here = $this->bookmarks['start'];
+			return substr( $this->html, $here->start, $here->end - $here->start + 1 );
+		}
+
+		// @TODO: Unique bookmark names
+		$this->set_bookmark( 'start' );
+		if ( ! $this->find_closing_tag() ) {
+			return false;
+		}
+		$this->set_bookmark( 'end' );
+
+		$start = $this->bookmarks['start']->start;
+		$end = $this->bookmarks['end']->end;
+		$inner_content = substr( $this->html, $start, $end - $start + 1 );
+		$this->seek( 'start' );
+
+		$this->release_bookmark( 'start' );
+		$this->release_bookmark( 'end' );
+
+		return $inner_content;
+	}
+
+	public function set_outer_content( $new_html ) {
+		if ( false === $this->fully_supported_input || false === $this->ensure_support() ) {
+			return false;
+		}
+
+		if ( ! $this->get_tag() || $this->is_tag_closer() ) {
+			return false;
+		}
+
+		$element = WP_HTML_Spec::element_info( $this->get_tag() );
+		// @TODO: Replace void and self-closing tags.
+		if ( $element::is_void || ( ! $element::is_html && $this->has_self_closing_flag() ) ) {
+			return false;
+		}
+
+		// @TODO: Unique bookmark names
+		$this->set_bookmark( 'start' );
+		if ( ! $this->find_closing_tag() ) {
+			return false;
+		}
+		$this->set_bookmark( 'end' );
+
+		$start = $this->bookmarks['start']->start;
+		$end = $this->bookmarks['end']->end + 1;
+		$this->lexical_updates[] = new WP_HTML_Text_Replacement( $start, $end, $new_html );
+		$this->get_updated_html();
+		$this->bookmarks['start']->start = $start;
+		$this->bookmarks['start']->end = $start;
+		$this->seek( 'start' );
+
+		$this->release_bookmark( 'start' );
+		$this->release_bookmark( 'end' );
 	}
 }
--- tests/phpunit/tests/html-api/Tests_HtmlApi_wpHtmlProcessor_Support.php
+++ tests/phpunit/tests/html-api/Tests_HtmlApi_wpHtmlProcessor_Support.php
@@ -23,6 +23,16 @@ function esc_attr( $s ) { return str_replace( [ '<', '>', '"' ], [ '&lt;', '&gt;
  * @coversDefaultClass WP_HTML_Processor
  */
 class Tests_HtmlApi_wpHtmlProcessor_Support extends WP_UnitTestCase {
+	private function html_processor_at_start( $html ) {
+		$p = new WP_HTML_Processor( $html );
+
+		while ( true !== $p->get_attribute( 'start' ) && $p->next_tag() ) {
+			continue;
+		}
+
+		return $p;
+	}
+
 	/**
 	 * @dataProvider data_fully_balanced_html
 	 */
@@ -85,14 +95,10 @@ class Tests_HtmlApi_wpHtmlProcessor_Support extends WP_UnitTestCase {
 	 * @dataProvider data_next_sibling
 	 */
 	public function test_finds_next_sibling( $html ) {
-		$p = new WP_HTML_Processor( $html );
-
-		while ( true !== $p->get_attribute( 'start' ) ) {
-			$p->next_tag();
-		}
+		$p = $this->html_processor_at_start( $html );
 		$p->next_sibling();
 
-		$this->assertTrue( $p->get_attribute( 'end' ) );
+		$this->assertTrue( $p->get_attribute( 'end' ), 'Did not finding sibling tag.' );
 	}
 
 	/**
@@ -100,8 +106,236 @@ class Tests_HtmlApi_wpHtmlProcessor_Support extends WP_UnitTestCase {
 	 */
 	public function data_next_sibling() {
 		return array(
+			'Leading markup'     => array( 'before<img start><img end><img>' ),
 			'Top-level siblings' => array( '<img start><img end><img>' ),
-			'List items'         => array( '<ul><li>One</li><li start>Two</lista><li end>Three</li><li>Four</li></ul>' ),
+			'Nested siblings'    => array( '<ul><li>One</li><li start>Two</li><li end>Three</li><li>Four</li></ul>' ),
+			'Nesting avalanche'  => array( '<img start><ul end><li><div><p><strong><a><img></a></strong></p></div></li></ul><div></div><footer></footer>'),
+		);
+	}
+
+	/**
+	 * @dataProvider data_no_next_sibling
+	 */
+	public function test_finds_no_next_sibling_when_none_exists( $html ) {
+		$p = $this->html_processor_at_start( $html );
+		$this->assertFalse( $p->next_sibling(), 'Found a sibling when none exists.' );
+	}
+
+	public function data_no_next_sibling() {
+		return array(
+			'Leading markup'        => array( 'before<div><img start></div><img end><img>' ),
+			'No more siblings'      => array( '<ul><li></li><li start></li></ul><ul><li end></li></ul>' ),
+			'Tag-closing avalanche' => array( '<ul><li><div><p><strong><a><img start></a></strong></p></div></li></ul><div end></div><footer></footer>'),
+		);
+	}
+
+	/**
+	 * @dataProvider data_next_child
+	 */
+	public function test_finds_next_child( $html ) {
+		$p = $this->html_processor_at_start( $html );
+
+		$p->next_child();
+		$this->assertTrue( $p->get_attribute( 'end' ), 'Did not find child tag.' );
+	}
+
+	public function data_next_child() {
+		return array(
+			'Leading markup' => array( 'this is not tag content<div start><img end></div>afterwards' ),
+			'Normal nesting' => array( '<ul><li></li><li><p start>text<img end></p></li><li></li></ul>' ),
+		);
+	}
+
+	/**
+	 * @dataProvider data_no_next_child
+	 */
+	public function test_finds_no_next_child( $html ) {
+		$p = $this->html_processor_at_start( $html );
+
+		$this->assertFalse( $p->next_child(), 'Did not find child tag.' );
+	}
+
+	public function data_no_next_child() {
+		return array(
+			'Leading markup' => array( 'this is not tag content<div start></div><img end>afterwards' ),
+			'Already nested' => array( '<li><li></li><li><p start>text</p><img end></li><li></li></ul>', ),
+		);
+	}
+
+	public function test_finds_chain_of_elements() {
+		$p = new WP_HTML_Processor( <<<HTML
+<main>
+	<h2>Things I could be eating right now</h2>
+	<ul>
+		<li>Apples</li>
+		<li>Pears</li>
+		<li><em>Prickly</em> pears</li>
+		<li>
+			<img src="yum.avif">
+			<details>
+				<summary>Scwarzwälder Kirschtorte</summary>
+				<ul>
+					<li>Flour</li>
+					<li>Eggs</li>
+					<li this-one>Sugar</li>
+					<li>Cream</li>
+					<li>Cherries</li>
+					<li>Chocolate</li>
+				</ul>
+			</details>
+		</li>
+	</ul>
+</main>
+HTML
+		);
+
+		$p->next_tag();
+		$p->next_child();
+		$p->next_sibling();
+		$p->next_child();
+		$p->next_sibling();
+		$p->next_sibling();
+		$p->next_sibling();
+		$p->next_child();
+		$p->next_sibling();
+		$p->next_child();
+		$p->next_sibling();
+		$p->next_child();
+		$p->next_sibling();
+		$p->next_sibling();
+
+		$this->assertTrue( $p->get_attribute( 'this-one' ) );
+	}
+
+	/**
+	 * @dataProvider data_inner_content
+	 */
+	public function test_get_inner_content( $before, $inner, $after ) {
+		$p = $this->html_processor_at_start( $before . $inner . $after );
+
+		$this->assertSame( $inner, $p->get_inner_content(), 'Found the wrong inner content.' );
+	}
+
+	public function data_inner_content() {
+		return array(
+			'Leading text' => array( '<!-- when will this start? --><div start>', 'text', '</div>' ),
+			'Single tag'   => array( '<div start>', 'text', '</div>' ),
+			'Nested tags'  => array( '<div start>', '<ul><li>One</li><li><strong>Two<img></strong></li></ul>', '</div>' ),
+			'Complex HTML' => array(
+				<<<HTML
+<main>
+	<h2>Things I could be eating right now</h2>
+	<ul>
+		<li>Apples</li>
+		<li>Pears</li>
+		<li><em>Prickly</em> pears</li>
+		<li>
+			<img src="yum.avif">
+			<details start>
+HTML,
+				<<<HTML
+				<summary>Scwarzwälder Kirschtorte</summary>
+				<ul>
+					<li>Flour</li>
+					<li>Eggs</li>
+					<li>Sugar</li>
+					<li>Cream</li>
+					<li>Cherries</li>
+					<li>Chocolate</li>
+				</ul>
+
+HTML,
+			<<<HTML
+</details>
+		</li>
+	</ul>
+</main>
+HTML
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider data_set_inner_content
+	 */
+	public function test_set_inner_content( $before, $old, $new, $after ) {
+		$p = $this->html_processor_at_start( $before . $old . $after );
+
+		$p->set_inner_content( $new );
+
+		$this->assertSame( $before . $new . $after, $p->get_updated_html(), 'Did not properly swap out inner content.' );
+	}
+
+	public function data_set_inner_content() {
+		return array(
+			'Single tag'  => array( '<div start>', 'boring text', 'exciting text', '</div>' ),
+			'Nested tags' => array( '<div><ul><li start>', '<p><img>This is <strong>neat</strong></p>', 'this<br>is<br>not', '</li></ul></div>' ),
+		);
+	}
+
+	/**
+	 * @dataProvider data_outer_content
+	 */
+	public function test_get_outer_content( $before, $outer, $after ) {
+		$p = $this->html_processor_at_start( $before . $outer . $after );
+
+		$this->assertSame( $outer, $p->get_outer_content(), 'Found the wrong outer content.' );
+	}
+
+	public function data_outer_content() {
+		return array(
+			'Leading text' => array( '<!-- when will this start? -->', '<div start>text</div>', 'when will it end?' ),
+			'Single tag'   => array( '', '<div start>text</div>', '' ),
+			'Nested tags'  => array( '<div>', '<ul start><li>One</li><li><strong>Two<img></strong></li></ul>', '</div>' ),
+			'Complex HTML' => array(
+				<<<HTML
+<main>
+	<h2>Things I could be eating right now</h2>
+	<ul>
+		<li>Apples</li>
+		<li>Pears</li>
+		<li><em>Prickly</em> pears</li>
+		<li>
+			<img src="yum.avif">
+
+HTML,
+				<<<HTML
+<details start>
+				<summary>Scwarzwälder Kirschtorte</summary>
+				<ul>
+					<li>Flour</li>
+					<li>Eggs</li>
+					<li>Sugar</li>
+					<li>Cream</li>
+					<li>Cherries</li>
+					<li>Chocolate</li>
+				</ul>
+			</details>
+HTML,
+			<<<HTML
+		</li>
+	</ul>
+</main>
+HTML
+			),
+		);
+	}
+
+	/**
+	 * @dataProvider data_set_outer_content
+	 */
+	public function test_set_outer_content( $before, $old, $new, $after ) {
+		$p = $this->html_processor_at_start( $before . $old . $after );
+
+		$p->set_outer_content( $new );
+
+		$this->assertSame( $before . $new . $after, $p->get_updated_html(), 'Did not properly swap out outer content.' );
+	}
+
+	public function data_set_outer_content() {
+		return array(
+			'Single tag'  => array( '', '<div start>boring text</div>', 'exciting text', '' ),
+			'Nested tags' => array( '<div><ul>', '<li start><p><img>This is <strong>neat</strong></p></li>', '<li>this is<br>not</li>', '<li></li></ul></div>' ),
 		);
 	}
 }
