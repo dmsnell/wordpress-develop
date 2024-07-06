2746122326fa554c96f8c0a7abaf1301dfbf082e
Dennis Snell <dennis.snell@automattic.com>
2024-07-06 03:31:48 -0700

    Undo added element support.

--- src/wp-includes/html-api/class-wp-html-open-elements.php
+++ src/wp-includes/html-api/class-wp-html-open-elements.php
@@ -237,9 +237,14 @@ class WP_HTML_Open_Elements {
 		return $this->has_element_in_specific_scope(
 			$tag_name,
 			array(
-				'APPLET',
-				'MARQUEE',
-				'OBJECT',
+
+				/*
+				 * Because it's not currently possible to encounter
+				 * one of the termination elements, they don't need
+				 * to be listed here. If they were, they would be
+				 * unreachable and only waste CPU cycles while
+				 * scanning through HTML.
+				 */
 			)
 		);
 	}
@@ -534,10 +539,7 @@ class WP_HTML_Open_Elements {
 		 * cases where the precalculated value needs to change.
 		 */
 		switch ( $item->node_name ) {
-			case 'APPLET':
 			case 'BUTTON':
-			case 'MARQUEE':
-			case 'OBJECT':
 				$this->has_p_in_button_scope = false;
 				break;
 
